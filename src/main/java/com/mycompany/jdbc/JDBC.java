0a91666c6de7c5480706d2d8ba9b2e8cef15d548
foreroivan130@gmail.com <foreroivan130@gmail.com>
2025-04-03 07:45:20 -0500

    Commit inicial del proyecto

---
+++ src/main/java/com/mycompany/jdbc/JDBC.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jdbc;
+
+/**
+ *
+ * @author SENA
+ */
+public class JDBC {
+
+    public static void main(String[] args) {
+       
+        
+    }
+}
